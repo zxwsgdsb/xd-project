d47ccd8ec4d603eff87614e64236200e56921eb0
Your Name <you@example.com>
2019-11-15 15:43:35 +0800

    运营管理端最终版补充

--- demo-3/src/main/java/com/datangedu/cn/dao/mapper/ShoppingMapper.java
+++ demo-3/src/main/java/com/datangedu/cn/dao/mapper/ShoppingMapper.java
@@ -2,6 +2,8 @@ package com.datangedu.cn.dao.mapper;
 
 import com.datangedu.cn.model.xd.shopping.Shopping;
 import com.datangedu.cn.model.xd.shopping.ShoppingExample;
+import com.datangedu.cn.util.ServiceShopping;
+
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -32,11 +34,7 @@ public interface ShoppingMapper {
     int updateByPrimaryKeySelective(Shopping record);
 
     int updateByPrimaryKey(Shopping record);
-<<<<<<< HEAD
 
 	List<ServiceShopping> selectByshopping();
 
-
-=======
->>>>>>> f3406570f14710d84bddf65eb135114d7a838180
 }
